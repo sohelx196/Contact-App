ee5ff7b833b0d05afec92f254209cff9d93508e9
sohelx196 <sohel.devloper196@gmail.com>
2024-12-10 15:56:52 +0530

    changed in main

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
   return (
     <div className='text-center flex flex-col justify-center h-screen'>
         <h1 className='text-slate-300 sm:text-base md:text-xl lg:text-3xl font-serif'>Hey there..Lets Explore Some Features !!</h1>
-
-    
+       
+           <h4 className='text-white p-16 '><marquee behavior="" direction="">Currently Working on it but you can use Todo..</marquee></h4>
+       
     </div>
   )
 }
